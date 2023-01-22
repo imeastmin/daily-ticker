cde6434eb915ccb331981c853aebedc8d5045de7
eastmin <imsenhaku@gmail.com>
2023-01-22 18:17:50 +0900

    feat: 회원삭제 기능 추가

---
+++ src/main/java/com/gym/ticket/GymTicketApplication.java
@@ -0,0 +1,13 @@
+package com.gym.ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymTicketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymTicketApplication.class, args);
+    }
+
+}
